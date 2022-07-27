8616d41aa033f34cc65d28d5ab1ce21fc86ddcb7
Suleiman Ibrahim <isuleimanibrahim@gmail.com>
2022-07-27 21:17:37 +0100

    changes

--- src/main/java/com/example/fashionblogapi/FashionBlogApiApplication.java
+++ src/main/java/com/example/fashionblogapi/FashionBlogApiApplication.java
@@ -11,3 +11,11 @@ public class FashionBlogApiApplication {
     }
 
 }
+
+//echo "# week-nine-docker" >> README.md
+//        git init
+//        git add README.md
+//        git commit -m "first commit"
+//        git branch -M main
+//        git remote add origin https://github.com/SuleimanIbrahiminuwa/week-nine-docker.git
+//        git push -u origin main
